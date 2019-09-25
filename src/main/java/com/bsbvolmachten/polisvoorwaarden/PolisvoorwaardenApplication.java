7b6e62cb5a593c3379844c86a63f9440132e4b46
Jelte Dirks <j.dirks@outlook.com>
2019-09-25 22:25:39 +0200

    initial

---
+++ src/main/java/com/bsbvolmachten/polisvoorwaarden/PolisvoorwaardenApplication.java
@@ -0,0 +1,18 @@
+package com.bsbvolmachten.polisvoorwaarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class PolisvoorwaardenApplication {
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PolisvoorwaardenApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolisvoorwaardenApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/bsbvolmachten/polisvoorwaarden/PolisvoorwaardenRestController.java
@@ -0,0 +1,48 @@
+package com.bsbvolmachten.polisvoorwaarden;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@RestController
+public class PolisvoorwaardenRestController {
+
+    @RequestMapping(value = "/api/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String upload(@RequestParam("file") MultipartFile file) {
+
+        File convertFile = new File("/var/tmp" + file.getOriginalFilename());
+
+        try {
+            convertFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println("error creating new file");
+            e.printStackTrace();
+            return "Something went wrong";
+        }
+
+        try {
+            FileOutputStream fOut = new FileOutputStream(convertFile);
+            fOut.write(file.getBytes());
+            fOut.close();
+        } catch (IOException e) {
+            System.out.println("error creating file output stream");
+            e.printStackTrace();
+        }
+
+        return "Uploaded";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "Home";
+    }
+
+}
