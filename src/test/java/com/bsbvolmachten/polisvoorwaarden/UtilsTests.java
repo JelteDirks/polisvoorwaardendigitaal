4fa1627466aebd426ce01f241b40bb48616388de
Jelte Dirks <j.dirks@outlook.com>
2019-09-26 23:15:53 +0200

    add bytes to hex string test

--- src/test/java/com/bsbvolmachten/polisvoorwaarden/UtilsTests.java
+++ src/test/java/com/bsbvolmachten/polisvoorwaarden/UtilsTests.java
@@ -16,7 +16,15 @@ public class UtilsTests {
     @Test
     public void sha256TextEncoding() throws NoSuchAlgorithmException {
         String testString = "123456";
+
         assertEquals("8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", Utils.sha256Encode(testString.getBytes()));
     }
 
+    @Test
+    public void bytesToHexStringTest() {
+        byte[] bytes = {-1, -86, -69};
+        String hex = "ffaabb";
+
+        assertEquals(hex, Utils.bytesToHexString(bytes));
+    }
 }
