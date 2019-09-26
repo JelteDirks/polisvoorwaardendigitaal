c8d4c7763b3b299ba1636bda08f13dac7b9755ca
Jelte Dirks <j.dirks@outlook.com>
2019-09-26 22:56:48 +0200

    add sha encoding with test

---
+++ src/main/java/com/bsbvolmachten/polisvoorwaarden/Utils.java
@@ -0,0 +1,27 @@
+package com.bsbvolmachten.polisvoorwaarden;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Utils {
+
+    public static String bytesToHexString(byte[] bytes) {
+
+        StringBuffer builder = new StringBuffer();
+
+        for (int i = 0; i < bytes.length; i++) {
+            builder.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return builder.toString();
+    }
+
+    public static String sha256Encode(byte[] bytes) throws NoSuchAlgorithmException {
+
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+
+        byte[] hashAsBytes = messageDigest.digest(bytes);
+
+        return bytesToHexString(hashAsBytes);
+    }
+}
---
+++ src/test/java/com/bsbvolmachten/polisvoorwaarden/UtilsTests.java
@@ -0,0 +1,22 @@
+package com.bsbvolmachten.polisvoorwaarden;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UtilsTests {
+
+    @Test
+    public void sha256TextEncoding() throws NoSuchAlgorithmException {
+        String testString = "123456";
+        assertEquals("8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", Utils.sha256Encode(testString.getBytes()));
+    }
+
+}
