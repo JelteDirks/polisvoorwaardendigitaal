8291a97fc76f62e0b5e42a50c7e365bd1960de5a
Jelte Dirks <j.dirks@outlook.com>
2019-09-26 23:39:57 +0200

    add pdf encryption test

--- src/main/java/com/bsbvolmachten/polisvoorwaarden/Utils.java
+++ src/main/java/com/bsbvolmachten/polisvoorwaarden/Utils.java
@@ -1,8 +1,6 @@
 package com.bsbvolmachten.polisvoorwaarden;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
--- src/test/java/com/bsbvolmachten/polisvoorwaarden/UtilsTests.java
+++ src/test/java/com/bsbvolmachten/polisvoorwaarden/UtilsTests.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
 
 import static org.junit.Assert.assertEquals;
@@ -17,7 +21,8 @@ public class UtilsTests {
     public void sha256TextEncoding() throws NoSuchAlgorithmException {
         String testString = "123456";
 
-        assertEquals("8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", Utils.sha256Encode(testString.getBytes()));
+        assertEquals("8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
+                Utils.sha256Encode(testString.getBytes()));
     }
 
     @Test
@@ -29,8 +34,15 @@ public class UtilsTests {
     }
 
     @Test
-    public void sha256PDFEncryptionTest() {
+    public void sha256PDFEncryptionTest() throws IOException, NoSuchAlgorithmException {
         String filePath = "./src/test/resources/Model CF2019 - Combi Autopolis.pdf";
+
+        Path pdfPath = Paths.get(filePath);
+
+        byte[] pdfBytes = Files.readAllBytes(pdfPath);
+
+        assertEquals("4d7fdf861d57149786a9cf5dab770998950dd44d299dc295e8af045904437e09",
+                Utils.sha256Encode(pdfBytes));
 
     }
 }
