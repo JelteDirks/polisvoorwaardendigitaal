3a62f21b62c0feee5ba33121ee8c8a9e0b62374e
Jelte Dirks <j.dirks@outlook.com>
2019-09-27 22:10:04 +0200

    add basic block properties

--- src/main/java/com/bsbvolmachten/polisvoorwaarden/blockchain/Block.java
+++ src/main/java/com/bsbvolmachten/polisvoorwaarden/blockchain/Block.java
@@ -1,4 +1,21 @@
 package com.bsbvolmachten.polisvoorwaarden.blockchain;
 
+import java.util.Date;
+
 public class Block {
+
+    private String blockHash;
+    private String prevBlockHash;
+    private long timestamp;
+    private int index;
+
+    private String documentHash;
+    private String documentVersion;
+    private String documentCode;
+    private String documentTitle;
+    private String companyCode;
+
+    public String generateDigest() {
+        return "";
+    }
 }
